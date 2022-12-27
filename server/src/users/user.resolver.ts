121119244efed5f58ac0cadf4e90f3e4f958dc46
Ahsan Ahmed <ahsanahmad779@outlook.com>
2022-12-27 18:02:07 +0500

    Validations added for user data

--- server/src/users/user.resolver.ts
+++ server/src/users/user.resolver.ts
@@ -1,5 +1,6 @@
 import { User } from "@prisma/client"
 import { Context } from "../server"
+import validator from "validator"
 
 interface Credentials {
     email: string
@@ -23,6 +24,54 @@ export const resolvers = {
     Mutation : {
         userSignup: async (_: any, {credentials, name, bio}: SignupArgs, { prisma }: Context) : Promise<UserPayload> => {
             const { email, password } = credentials;
+
+            const isValidEmail = validator.isEmail(email);
+            const isValidPassword = validator.isLength(password, {
+                min: 8
+            });
+
+            if(!isValidEmail) {
+                return {
+                    userErrors: [{
+                        message: "Provide a valid email"
+                    }],
+                    user: null
+                }
+            }
+
+            if(!isValidPassword) {
+                return {
+                    userErrors: [{
+                        message: "Password length is too short. Must be of 8 characters"
+                    }],
+                    user: null
+                }
+            }
+
+            if(!name) {
+                return {
+                    userErrors: [{
+                        message: "Provide a valid name"
+                    }],
+                    user: null
+                }
+            }
+
+            const emailExists = await prisma.user.findUnique({
+                where: {
+                    email
+                }
+            });
+
+            if (emailExists) {
+                return {
+                        userErrors: [{
+                            message: "Email already exists"
+                        }],
+                        user: null
+                    }
+            }
+
             const user = await prisma.user.create({
                 data: {
                     name,
